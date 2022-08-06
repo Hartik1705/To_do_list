a4df1a7c755589c4031978d5ef254e6755cf1008
Hartik Luhar <luharhartik2002@gmail.com>
2022-08-06 16:42:35 +0530

    Final changes

--- app.js
+++ app.js
@@ -175,11 +175,8 @@ app.post("/delete", function(req,res){
         res.redirect("/");
 })
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-    port = 3000;
-}
+const port = Process.env.PORT || 3000 ;
 
-app.listen(port, function(){ 
-    console.log("server is running on port 3000")
-})
+server.listen(port,()=>{  
+    console.log("server listening to port "+port);
+});
